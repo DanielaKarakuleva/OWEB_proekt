6469015bc8bc39f539842b6fe56d63c21e93bd71
Daniela Karakuleva <40542169+DanielaKarakuleva@users.noreply.github.com>
2021-11-17 15:45:09 +0100

    Add files via upload

--- welcome.js
+++ welcome.js
@@ -5,17 +5,17 @@ var hour = now.getHours();
 name = window.prompt("Please enter your name");
 
 if(hour<12)
-document.writeln("Good morning ");
+document.writeln("<h1>Good morning ");
 
 if(hour>=12)
 {
     hour =  hour - 12;
 
     if(hour<6)
-    document.writelm("Good afternoon ");
+    document.writeln("<h1>Good afternoon ");
 
-    if (hour>6)
-    document.writeln("Good evening ");
+    if (hour>=6)
+    document.writeln("<h1>Good evening ");
 }
 
-document.writeln(name)
+document.writeln(name + "<h1>");
